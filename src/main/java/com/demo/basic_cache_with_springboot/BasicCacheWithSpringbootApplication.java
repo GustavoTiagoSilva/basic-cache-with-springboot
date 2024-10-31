05321ef81ec852235dce7a887c7af803618f9bc2
Gustavo Silva <Gustavo_Silva@McKinsey.com>
2024-10-31 16:44:12 -0300

    Initial Commit

---
+++ src/main/java/com/demo/basic_cache_with_springboot/BasicCacheWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.demo.basic_cache_with_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCacheWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCacheWithSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/basic_cache_with_springboot/BasicCacheWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.basic_cache_with_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCacheWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
