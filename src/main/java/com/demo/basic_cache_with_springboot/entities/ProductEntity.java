aeffa482d38e47f586856bb6170d863dcc094529
Gustavo Silva <Gustavo_Silva@McKinsey.com>
2024-10-31 17:02:40 -0300

    build: map Product Entity and create SQL script to insert fake data into table

---
+++ src/main/java/com/demo/basic_cache_with_springboot/entities/ProductEntity.java
@@ -0,0 +1,53 @@
+package com.demo.basic_cache_with_springboot.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name = "PRODUCTS")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private String description;
+
+    public ProductEntity() {
+
+    }
+
+    public ProductEntity(UUID id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
